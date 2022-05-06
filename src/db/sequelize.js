122689c9bec7640d1aec8114c558fb3f1acb5964
StephaneCash <kikonistephane@gmail.com>
2022-05-06 12:36:32 +0200

    updatefile

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,6 +2,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 const PkemonModel = require('../models/pokemon');
 const UserModel = require('../models/user');
 const pokemons = require('./pocekmonList');
+const bcrypt = require('bcrypt')
 
 const sequelize = new Sequelize(
     'pokemons', 'root', '',
@@ -19,20 +20,23 @@ const Pokemon = PkemonModel(sequelize, DataTypes);
 const User = UserModel(sequelize, DataTypes);
 
 const initDB = () => {
-    return sequelize.sync({ force: true })
-        .then(rep => {
-            pokemons.map((pokemon) => {
-                Pokemon.create({
-                    name: pokemon.name,
-                    hp: pokemon.hp,
-                    cp: pokemon.cp,
-                    picture: pokemon.picture,
-                    types: pokemon.types
-                }).then(bulbizarre => console.log(bulbizarre.toJSON()))
-            })
-        });
+    return sequelize.sync({ force: true }).then(rep => {
+        pokemons.map((pokemon) => {
+            Pokemon.create({
+                name: pokemon.name,
+                hp: pokemon.hp,
+                cp: pokemon.cp,
+                picture: pokemon.picture,
+                types: pokemon.types
+            }).then(bulbizarre => console.log(bulbizarre.toJSON()))
+        })
+
+        bcrypt.hash('stephcash', 10)
+            .then(hash => User.create({ username: 'steph', password: hash }))
+            .then(user => console.log(user.toJSON()))
+    });
 }
 
 module.exports = {
-    initDB, Pokemon
+    initDB, Pokemon, User
 }

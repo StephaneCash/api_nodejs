04e8df9ee974e136bb4cd5693cf02179b32218dd
StephaneCash <kikonistephane@gmail.com>
2022-05-12 17:03:08 +0200

    sd

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,13 +2,13 @@ const { User } = require('../db/sequelize');
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken');
 const primaryKey = require('../auth/private_key');
- 
+
 module.exports = (app) => {
     app.post('/api/login', (req, res) => {
         User.findOne({
             where: { username: req.body.username }
         }).then((user => {
-     
+
             if (!user) {
                 const message = 'L\'utilisateur demandé n\'existe pas';
                 return res.status(400).json({ message })
@@ -18,22 +18,22 @@ module.exports = (app) => {
                     const message = 'Le mot de passe est incorrect';
                     return res.status(401).json({ message })
                 }
-         
+
                 // Création du jéton pour chaque user
                 const jeton = jwt.sign(
-                    { userId: user.id }, 
+                    { userId: user.id },
                     primaryKey,
                     { expiresIn: '24' }
                 )
-  
+
                 const message = `L'utilisateur a été connecté avec succès`;
                 return res.json({ message, data: user, jeton })
-            })  
-        }))  
-  
+            })
+        }))
+
             .catch(err => {
                 const message = `L'utilisateur n'a pas pu être connecté`;
                 return res.json({ message, data: err })
-            }) 
+            })
     })
 }   

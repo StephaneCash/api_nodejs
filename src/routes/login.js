d3d44a62f359b26a8bca4d4315e97c82f5f960d0
StephaneCash <kikonistephane@gmail.com>
2022-05-12 17:07:23 +0200

    Création du jéton pour chaque user

--- src/routes/login.js
+++ src/routes/login.js
@@ -8,32 +8,32 @@ module.exports = (app) => {
         User.findOne({
             where: { username: req.body.username }
         }).then((user => {
-   
+
             if (!user) {
                 const message = 'L\'utilisateur demandé n\'existe pas';
                 return res.status(400).json({ message })
-            }   
+            }
             bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
                 if (!isPasswordValid) {
                     const message = 'Le mot de passe est incorrect';
                     return res.status(401).json({ message })
-                }  
-  
+                }
+
                 // Création du jéton pour chaque user avec jwt
                 const jeton = jwt.sign(
                     { userId: user.id },
                     primaryKey,
                     { expiresIn: '24' }
-                )  
-   
+                )
+
                 const message = `L'utilisateur a été connecté avec succès`;
                 return res.json({ message, data: user, jeton })
-            })   
-        })) 
+            })
+        }))
 
             .catch(err => {
                 const message = `L'utilisateur n'a pas pu être connecté`;
                 return res.json({ message, data: err })
             })
-    }) 
+    })
 }   

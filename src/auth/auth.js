0b325a62bc5804fad66435ec11420c552f8f22ab
StephaneCash <kikonistephane@gmail.com>
2022-05-13 14:50:35 +0200

    middleware

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -3,4 +3,8 @@ const primaryKey = require('./private_key')
 
 module.exports = (req, res, next) => {
     const autorization = req.headers.autorization;
+
+    if(!autorization){
+        const msg = `Vous n'avez pas fourni le jéton d'authentification. Ajoutez-en un dans l'en-tête  dela requête`;
+    }
 }

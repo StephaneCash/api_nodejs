691ac0c1533f7506a703080cacc97c4444d6659e
StephaneCash <kikonistephane@gmail.com>
2022-05-23 13:06:53 +0200

    delete morgan

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const sequelize = require('./src/db/sequelize');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(morgan('dev'))
-    .use(bodyParser.json())
+app.use(bodyParser.json())
 
 sequelize.initDB()
 
